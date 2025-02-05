742977b0c7218d88d9225e7335336236713de6c0
Alexis <alexiscristiano2004@gmail.com>
2025-02-05 11:52:34 +0100

    user

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TodoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,21 @@ class Todo
      * @ORM\Column(type="boolean", nullable=true)
      */
     private $checked;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="todos")
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=User::class, mappedBy="todos")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +69,45 @@ class Todo
     public function setChecked(?bool $checked): self
     {
         $this->checked = $checked;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->addTodo($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            $user->removeTodo($this);
+        }
 
         return $this;
     }
